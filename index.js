834a52fb484fe07907f1bb7e6e3887fcee706798
methodius pkemoi <pkemoimethodius@gmil.com>
2024-06-27 11:48:31 +0300

    Adds the function tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity=`roller-skate`){
+
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork=function (hobbie='go to the office'){
+
+    return `This Monday, I will ${hobbie}.`
+}
+function wrapAdjective(parameter="*"){
+    return function(geek=`special`){
+        return (`You are ${parameter}${geek}${parameter}!`);
+    }
+}
